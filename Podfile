970635337e73a8465af3eb66333fb5a177a627bb
mhlee80 <myeonghyeon.lee@gmail.com>
2020-03-02 13:38:34 +0900

    setup cocoapods and logger

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.0'
+
+target 'SNS-Login' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SNS-Login
+  pod 'SwiftyBeaver'
+  pod 'SnapKit', '~> 5.0.0'
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+end
--- SNS-Login/AppDelegate.swift
+++ SNS-Login/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import SwiftyBeaver
+
+let log = SwiftyBeaver.self
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    log.addDestination(ConsoleDestination())
     return true
   }
 
