b67d80d8b57231e22a30c8e936e2181a23462ad2
mhlee80 <myeonghyeon.lee@gmail.com>
2020-09-10 14:18:04 +0900

    Firebase 초기화 명령을 앱 시작으로 옮김

--- SNS-Login/AppDelegate.swift
+++ SNS-Login/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftyBeaver
+import Firebase
 
 let log = SwiftyBeaver.self
 
@@ -19,8 +20,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     log.addDestination(ConsoleDestination())
-
     log.info("")
+    
+    FirebaseApp.configure()
     
     // Google Login
     GoogleLoginService.shared.handleApplication(application,
--- SNS-Login/Services/GoogleLoginService.swift
+++ SNS-Login/Services/GoogleLoginService.swift
@@ -27,7 +27,6 @@ class GoogleLoginService: NSObject {
   }
   
   func handleApplication(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) {
-    FirebaseApp.configure()
     GIDSignIn.sharedInstance()?.clientID = FirebaseApp.app()?.options.clientID
     GIDSignIn.sharedInstance()?.delegate = self
   }
